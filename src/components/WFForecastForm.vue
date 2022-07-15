a33accb52aea3c40f1a35aedeff72d756f629606
ssamilg <samil-98@hotmail.com>
2022-07-15 19:43:51 +0300

    custom form component created

---
+++ src/components/WFForecastForm.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  name: 'WFForecastForm',
+  data() {
+    return {
+      formData: {
+        name: null,
+        city: null,
+      },
+    };
+  },
+};
+</script>
+
+<template>
+  <div id="wf-forecast-form">
+    <v-layout>
+      <v-text-field
+        v-model="formData.name"
+        label="Name"
+      />
+    </v-layout>
+
+    <v-layout>
+      <v-text-field
+        v-model="formData.city"
+        label="City"
+      />
+    </v-layout>
+
+    <v-layout justify-end>
+      <v-btn>
+        Next
+      </v-btn>
+    </v-layout>
+  </div>
+</template>
+
+<style lang="scss">
+#wf-forecast-form {
+  height: 100%;
+}
+</style>
