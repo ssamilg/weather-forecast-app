2383fea3b52c7620797785d5f2ca41fd9e411d5f
ssamilg <samil-98@hotmail.com>
2022-07-17 22:05:07 +0300

    scrollbar styles updated accordingly to desing

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,4 @@
-<template>
-  <v-app>
-    <router-view/>
-  </v-app>
-</template>
-
 <script>
-
 export default {
   name: 'App',
 
@@ -14,3 +7,32 @@ export default {
   }),
 };
 </script>
+
+<template>
+  <v-app>
+    <router-view/>
+  </v-app>
+</template>
+
+<style lang="scss">
+::-webkit-scrollbar {
+  width: 6px;
+}
+
+::-webkit-scrollbar-track {
+  background: #f5f5f5;
+  border-radius: 50px;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #e0e0e0;
+  border-radius: 50px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #c9c9c9;
+  border-radius: 50px;
+}
+</style>
