83ed7624c7733db76675c04db8b0c4367063084e
ssamilg <samil-98@hotmail.com>
2022-07-18 02:34:47 +0300

    current weather styles added for indicating current weather

--- src/components/WFTodaysWeatherSection.vue
+++ src/components/WFTodaysWeatherSection.vue
@@ -7,9 +7,18 @@ export default {
       required: true,
     },
     todaysForecast: {
-      type: Object,
+      type: Array,
       required: true,
     },
+  },
+  data() {
+    return {
+      todaysForecastData: null,
+    };
+  },
+  mounted() {
+    this.todaysForecastData = this.todaysForecast
+      .map((i) => (i.dt_txt === this.currentWeather.dt_txt ? { ...i, isCurrent: true } : i));
   },
 };
 </script>
@@ -22,20 +31,22 @@ export default {
       </v-flex>
     </v-layout>
 
-    <v-layout class="todays-weather-container">
+    <v-layout align-center no-wrap class="todays-weather-container">
       <v-flex
-        v-for="todaysWeather in todaysForecast"
+        v-for="todaysWeather in todaysForecastData"
         :key="todaysWeather.dt_txt"
         class="todays-weather"
+        :class="todaysWeather.isCurrent && 'current-weather'"
       >
         <v-layout>
-          <v-flex>
-            {{ new Date(todaysWeather.dt_txt).getHours().toString().padStart(2, '0') }}
+          <v-flex class="py-1">
+            {{ todaysWeather.isCurrent ?
+            'NOW' : new Date(todaysWeather.dt_txt).getHours().toString().padStart(2, '0') }}
           </v-flex>
         </v-layout>
 
-        <v-layout class="todays-weather-icons">
-          <v-flex>
+        <v-layout justify-center class="todays-weather-icons">
+          <v-flex shrink>
             <v-img
               :src="`http://openweathermap.org/img/wn/${todaysWeather.weather[0].icon}@2x.png`"
               height="48"
@@ -59,6 +70,17 @@ export default {
   .todays-weather-container {
     overflow: auto;
 
+    .current-weather {
+      // border: 1px solid #e0e0e0 !important;
+      background-color: #FFECB3;
+      border-radius: 8px;
+
+      .todays-weather-temperature {
+        font-weight: 500;
+        font-size: 1.5rem;
+      }
+    }
+
     .todays-weather {
       text-align: center;
       border: 1px solid rgba($color: #000000, $alpha: 0);
@@ -72,8 +94,8 @@ export default {
       }
 
       &-temperature {
-        font-weight: 500;
-        font-size: 1.5rem;
+        font-weight: 300;
+        font-size: 1rem;
       }
 
       &-icons {
